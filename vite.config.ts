abcf57ed0e99a882598213df803922fec0d6f9c3
ValhallanEsports <valhallanwindmilllv@gmail.com>
2026-07-10 10:15:10 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // <-- CHANGED THIS TO RELATIVE ROUTING
+    base: '/Test-Game/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
